66dfe871e869eb2ddc27f112d1d192455ef2e710
David Vandal <dsv101@gmail.com>
2015-05-04 20:23:19 -0400

    added anti-aliasing

--- shader/fsFinal.glsl
+++ shader/fsFinal.glsl
@@ -4,12 +4,22 @@ precision highp float;
 
 uniform sampler2D tex;
 uniform vec4 tex_size;
+uniform vec2 resolution;
 
 varying vec2 texpos;
 
+varying vec2 v_rgbNW;
+varying vec2 v_rgbNE;
+varying vec2 v_rgbSW;
+varying vec2 v_rgbSE;
+varying vec2 v_rgbM;
+
+#include "shader/fxaa.glsl"
+
 void main()
 {
-	vec3 tc = texture2D(tex, texpos.st).rgb;
+	vec4 antiAliased = fxaa(tex, texpos.st * resolution, resolution, v_rgbNW, v_rgbNE, v_rgbSW, v_rgbSE, v_rgbM);
+	vec3 tc = antiAliased.rgb;
 	
 	gl_FragColor.rgb = tc;
 	gl_FragColor.a = 1.0;
--- shader/vsFinal.glsl
+++ shader/vsFinal.glsl
@@ -1,9 +1,21 @@
 uniform sampler2D tex;
+uniform vec2 resolution;
 attribute vec3 a_position;
 attribute vec2 a_coord;
 varying vec2 texpos;
 
+varying vec2 v_rgbNW;
+varying vec2 v_rgbNE;
+varying vec2 v_rgbSW;
+varying vec2 v_rgbSE;
+varying vec2 v_rgbM;
+
+#include "shader/texcoords.glsl"
+
 void main(){
 	texpos = a_coord;	
 	gl_Position = vec4(a_position, 1.0);
+	
+	vec2 fragCoord = a_coord * resolution;
+	texcoords(fragCoord, resolution, v_rgbNW, v_rgbNE, v_rgbSW, v_rgbSE, v_rgbM);
 }
